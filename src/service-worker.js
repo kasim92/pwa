f7782694784b87ba1e0d61259827672bc6bec40d
kasim92 <1992kasimm@gmail.com>
2021-04-20 12:36:19 +0300

    first

---
+++ src/service-worker.js
@@ -0,0 +1,72 @@
+workbox.core.setCacheNameDetails({ prefix: "pwa" });
+workbox.routing.registerNavigationRoute('/index.html')
+
+// workbox.routing.registerRoute(
+//   new RegExp('https://priceless-morse-f661d5.netlify.app'),
+//   new workbox.strategies.NetworkFirst()
+// );
+// workbox.routing.registerRoute(
+//   /.*\.(?:js|woff|woff2|otf|ttf)(?:$|\?)/,
+//   workbox.strategies.networkFirst({
+//     cacheName: 'assets-cache',
+//     plugins: [
+//       new workbox.expiration.Plugin({})
+//     ],
+//   })
+// )
+
+// workbox.routing.registerRoute(
+//   /\.(?:png|gif|jpg|jpeg|svg)$/,
+//   workbox.strategies.staleWhileRevalidate({
+//     cacheName: 'images',
+//     plugins: [
+//       new workbox.expiration.Plugin({
+//         maxEntries: 60,
+//         maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+//       }),
+//     ],
+//   }),
+// );
+// workbox.routing.registerRoute(
+//   /\.(?:js|css)$/,
+//   workbox.strategies.staleWhileRevalidate({
+//     cacheName: 'static-resources',
+//   })
+// );
+
+
+// workbox.routing.registerRoute(
+//   new RegExp('https://priceless-morse-f661d5.netlify.app/orders'),
+//   workbox.strategies.networkFirst({
+//     cacheName: 'api',
+//   }),
+// );
+
+// workbox.routing.registerRoute(
+//   new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+//   workbox.strategies.cacheFirst({
+//     cacheName: 'googleapis',
+//     plugins: [
+//       new workbox.expiration.Plugin({
+//         maxEntries: 30,
+//       }),
+//     ],
+//   }),
+// );
+
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+//  workbox.precaching.suppresWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,35 @@
+const path = require('path');
+
 module.exports = {
-  transpileDependencies: [
-    'vuetify', "@koumoul/vjsf"
-  ]
+  "transpileDependencies": [
+    "vuetify"
+  ],
+  pwa: {
+    name: 'https://peaceful-bhabha-90ff47.netlify.app/',
+    themeColor: '#2d3748',
+    msTileColor: '#2d3748',
+    appleMobileWebAppCapable: 'no',
+    appleMobileWebAppStatusBarStyle: 'default',
+    manifestPath: 'manifest.json',
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'src/service-worker.js',
+      // ...other Workbox options...
+      exclude: [
+        /\.map$/,
+        /manifest\.json$/
+      ],
+    }
+  },
+  // productionSourceMap: process.env.NODE_ENV != 'production',
+  // parallel: false,
+  // configureWebpack: {
+  //   resolve: {
+  //     extensions: ['.js', '.vue', '.json'],
+  //     alias: {
+  //       '@': path.resolve('src'),
+  //     }
+  //   }
+  // }
 }
